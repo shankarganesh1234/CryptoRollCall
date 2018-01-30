136a645920ec1d3d582d66d1aa00ff0f08148020
shankarganesh1234 <shanganesh@ebay.com>
2018-01-30 13:31:26 -0800

    howto page UI changes

--- src/app/crypto-howto/crypto-howto.component.ts
+++ src/app/crypto-howto/crypto-howto.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {Title} from "@angular/platform-browser";
 
 declare const $: any;
+declare const AOS:any;
 @Component({
     selector: 'crypto-about',
     templateUrl: `./crypto-howto.component.html`,
@@ -14,6 +15,7 @@ export class CryptoHowToComponent implements OnInit{
     isGetStarted: boolean  = false;
 
     constructor(private route: ActivatedRoute, private router: Router,private titleService: Title) {
+        AOS.init();
         route.params.subscribe(val => {
             // hide the welcome message
             $('#welcomeMessage').hide();
@@ -22,6 +24,10 @@ export class CryptoHowToComponent implements OnInit{
 
     ngOnInit(): void {
         this.titleService.setTitle('CryptoRollCall - Questions regarding crypto currencies');
+        $(window).on('load', function() {
+            AOS.refresh();
+        });
     }
+
 }
 
